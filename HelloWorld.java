94911e99110a29d454741c7bbd4f44395f179aa3
blam19 <blam19@calpoly.edu>
2021-04-13 20:27:54 -0700

    partner

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,8 @@
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
-    	   System.out.println("This is my 4th year, last quarter");
-	       System.out.println("Kenneth Liu");
+    	System.out.println("This is my 4th year, last quarter");
+	System.out.println("Kenneth Liu");
+	System.out.println("This is Brandon, Kenneth's partner");
     }
 }
